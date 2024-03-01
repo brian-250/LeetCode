49e11e69f2edd9694f0a771d172506c4f92524ce
Brian Milian <brianmilian@csu.fullerton.edu>
2024-03-01 02:50:51 -0800

    Add solution for Leet Code #206, 'Reverse Linked List'

---
+++ Reversing a Singly Linked List/README.md
@@ -0,0 +1,9 @@
+# Reverse a Singly Linked List
+
+This program contains my solution for reversing a singly linked list in Leet Code
+
+## Problem
+
+<div align="center">
+  <img src="reverse-sll.png" alt="questions from project 4" width="1500px" />
+</div>
---
+++ Reversing a Singly Linked List/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) {
+            return head;
+        }
+        ListNode* prev = nullptr;
+        ListNode* cur = head;
+        ListNode* nxt = cur->next;
+        while (nxt != nullptr) {
+            cur->next = prev;
+            prev = cur;
+            cur = nxt;
+            nxt = cur->next;
+        }
+        if (nxt == nullptr) {
+            cur->next = prev;
+            head = cur;
+        }
+        return head;
+    }
+};
